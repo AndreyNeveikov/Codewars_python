25bcab25fb4764013a382cfe7e8f53dea3ecd5a6
Andrey <94782387+AndreyNeveikov@users.noreply.github.com>
2023-05-31 10:40:02 +0300

    add solution on problem №876

--- Codewars.py
+++ Codewars.py
@@ -2,3 +2,14 @@
 This file here just for tmp tests
 All tasks in 'Codewars.ipynb'
 """
+
+
+class Solution(object):
+    @staticmethod
+    def middle_node(head):
+        middle_node_index = int(round(len(head) / 2, 0))
+        return head[middle_node_index:]
+
+
+head = [1, 2, 3, 4, 5, 6]
+print(Solution.middle_node(head))
